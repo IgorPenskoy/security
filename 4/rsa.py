e867fde1db0f49472aeae88dc7ca759afd9cc9e7
igorpenskoy <igorpenskoy@yandex.ru>
2016-10-19 00:35:07 +0300

    Perform stubs for encrypt/decrypt

--- 4/rsa.py
+++ 4/rsa.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 import argparse
+from bitarray import bitarray
+
+
+def rsa_encrypt(bits, key):
+    return bits
+
+
+def rsa_decrypt(bits, key):
+    return bits
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RSA algorithm utility')
@@ -8,3 +17,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
     path = args.path
     to_decrypt = args.d
+    file = open(path, 'rb')
+    text = bitarray()
+    text.fromfile(file)
+    file.close()
+    if to_decrypt:
+        print('Start decryption for file:', path)
+        text = rsa_decrypt(text, 65537)
+        print('Finish decryption.')
+    else:
+        print('Start encryption for file:', path)
+        text = rsa_encrypt(text, 65537)
+        print('Finish encryption.')
+    file = open(path, 'wb')
+    file.write(text.tobytes())
+    file.close()
